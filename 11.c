47122964b8ebcb7ff7032414cb1587eb6076b242
roadtrip0108-oss <roadtrip0108@gmail.com>
2025-11-16 09:39:28 +0900

    11_6

--- 11.c
+++ 11.c
@@ -3,24 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
-  int grade[5];
-  int sum=0;
+  int i=100;
+  int *p=&i;
+  int **q = &p;
   
-  for(i=0;i<5;i++)
-  {
-     printf("grade[%i] = ",i);
-     scanf("%d", &grade[i]);
-  }
+  *p = 200;
+  printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
   
-  for (i=0;i<5;i++)
-  {
-      printf("grade[%i] =", i, *(grade+i));
-      sum = sum + *(grade+i);
-  }
-  
-  printf("Average : %i\n", sum/5);
-  
+  **q = 300;
+  printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);  
   
   system("PAUSE");	
   return 0;
